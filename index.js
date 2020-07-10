cf9a33f3ac0bc097d6d70b57c98218f8724a970f
Deena57017 <Deena57017@gmail.com>
2020-07-10 17:35:54 +0700

    1

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const port = process.env.PORT || 4000;
+// Import the appropriate class
+const { WebhookClient } = require('dialogflow-fulfillment');
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.get('/', (req, res) => {
+    res.send({
+        success: true
+    });
+})
+
+app.post('/webhook', (req, res) => {
+    console.log('POST: /');
+    console.log('Body: ', req.body);
+//Create an instance
+    const agent = new WebhookClient({
+        request: req,
+        response: res
+    });
+//Test get value of WebhookClient
+    console.log('agentVersion: ' + agent.agentVersion);
+    console.log('intent: ' + agent.intent);
+    console.log('locale: ' + agent.locale);
+    console.log('query: ', agent.query);
+    console.log('session: ', agent.session);
+//Function Location
+    function randomNumber(agent) {
+        let startNumber = req.body.queryResult.parameters.startNumber
+        let endNumber = req.body.queryResult.parameters.endNumber
+
+        let result = parseInt(Math.random() * (endNumber - startNumber) + startNumber);
+
+        agent.add(`Random number between ${startNumber} and ${endNumber} is ${result}`);
+    }
+// Run the proper function handler based on the matched Dialogflow intent name
+    let intentMap = new Map();
+    intentMap.set('Number', randomNumber);  // "Location" is once Intent Name of Dialogflow Agent
+    agent.handleRequest(intentMap);
+});
+app.listen(port, () => {
+    console.log(`Server is running at port: ${port}`);
+});
